6a159fb4d254c8ddb62ded85695ee805c798049f
Frederik Höft <43813142+frederik-hoeft@users.noreply.github.com>
2026-08-03 12:52:20 +0200

    allow repeated collection-aware validation attributes

--- Source/Cyborg.Core.Aot/Modules/Validation/Attributes/VariableIdentifierAttribute.cs
+++ Source/Cyborg.Core.Aot/Modules/Validation/Attributes/VariableIdentifierAttribute.cs
@@ -1,4 +1,4 @@
 ﻿namespace Cyborg.Core.Aot.Modules.Validation.Attributes;
 
-[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = true)]
 internal sealed class VariableIdentifierAttribute : PropertyValidationAttribute;
