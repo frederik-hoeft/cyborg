9cee1a8bacd9309fb5f9d75f6bf9403956020bac
Frederik Höft <43813142+frederik-hoeft@users.noreply.github.com>
2026-08-03 12:51:51 +0200

    allow repeated collection-aware validation attributes

--- Source/Cyborg.Core.Aot/Modules/Validation/Attributes/MinLengthAttribute.cs
+++ Source/Cyborg.Core.Aot/Modules/Validation/Attributes/MinLengthAttribute.cs
@@ -1,6 +1,6 @@
 ﻿namespace Cyborg.Core.Aot.Modules.Validation.Attributes;
 
-[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = true)]
 internal sealed class MinLengthAttribute(int min) : PropertyValidationAttribute
 {
     public int Min { get; } = min;
