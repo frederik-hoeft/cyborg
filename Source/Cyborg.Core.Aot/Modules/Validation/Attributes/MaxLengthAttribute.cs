4e3c7f351df8d2865beca6c2514fa05c9193b18d
Frederik Höft <43813142+frederik-hoeft@users.noreply.github.com>
2026-08-03 12:52:01 +0200

    allow repeated collection-aware validation attributes

--- Source/Cyborg.Core.Aot/Modules/Validation/Attributes/MaxLengthAttribute.cs
+++ Source/Cyborg.Core.Aot/Modules/Validation/Attributes/MaxLengthAttribute.cs
@@ -1,6 +1,6 @@
 ﻿namespace Cyborg.Core.Aot.Modules.Validation.Attributes;
 
-[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = true)]
 internal sealed class MaxLengthAttribute(int max) : PropertyValidationAttribute
 {
     public int Max { get; } = max;
